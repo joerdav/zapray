6b57d21f2fde90f2308d1a90abd0b4248c62d7a5
Joe-Davidson1802 <joe.davidson@infinityworks.com>
2021-09-22 12:15:10 +0100

    feat: middleware + logger

--- logger.go
+++ logger.go
@@ -2,11 +2,11 @@ package zapray
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/aws/aws-xray-sdk-go/xray"
 	"go.uber.org/zap"
 )
-
 
 // ZaprayLogger is a wrapper for zap.Logger, exposes the zap.Logger functions and adds the ability to Trace logs.
 type ZaprayLogger struct {
@@ -41,5 +41,27 @@ func (zprl *ZaprayLogger) Trace(ctx context.Context) *zap.Logger {
 		return zprl.Logger
 	}
 	traceId := xray.TraceID(ctx)
+	return zprl.Logger.With(zap.String("@xrayTraceId", traceId), zap.String("@xraySegmentId", seg.ID))
+}
+
+// TraceRequest creates a new zap.Logger but with the xrayTraceId and xraySegmentId baked in.
+//   log.TraceRequest(r).Info("myLog")
+//
+// Once trace is called you can use it as a zap.Logger.
+//
+//   tracedLogger := log.Trace(r)
+//   tracedLogger.Info("Log one")
+//   tracedLogger.Info("Log two")
+//
+// This means as above you can trace once and use the provided logger.
+func (zprl *ZaprayLogger) TraceRequest(r *http.Request) *zap.Logger {
+	if zprl == nil {
+		return nil
+	}
+	seg := xray.GetSegment(r.Context())
+	if seg == nil {
+		return zprl.Logger
+	}
+	traceId := xray.TraceID(r.Context())
 	return zprl.Logger.With(zap.String("@xrayTraceId", traceId), zap.String("@xraySegmentId", seg.ID))
 }
---
+++ middleware.go
@@ -0,0 +1,26 @@
+package zapray
+
+import (
+	"net/http"
+
+	"github.com/aws/aws-xray-sdk-go/xray"
+)
+
+// NewMiddlware creates X-Ray middleware that creates a subsegment
+// for each HTTP request.
+func NewMiddleware(next http.Handler) Middleware {
+	return Middleware{
+		Next: next,
+	}
+}
+
+// Middlware applies X-Ray segements to the wrapped handler.
+type Middleware struct {
+	Next http.Handler
+}
+
+func (h Middleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx, seg := xray.BeginSubsegment(r.Context(), "httpHandler")
+	h.Next.ServeHTTP(w, r.WithContext(ctx))
+	seg.Close(nil)
+}
